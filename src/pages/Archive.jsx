b2f30b274e00e6de738026cf2693ae265244ce95
ayyappansuruttaiyan <ayyappan.suruttaiyan@gmail.com>
2023-11-24 22:44:49 +0530

    bug fixed in archive page

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Archive(notes, tasks) {
-  return (
-    <div>
-      You have {notes.length} Notes and {tasks.length}. But, archived items.
-    </div>
-  );
+  return <div>You have 0 archived items.</div>;
 }
 
 export default Archive;
